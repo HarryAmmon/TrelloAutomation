b7c3b8b10cd238619a3c1a239b41650330afec32
HarryAmmon <harry_a99@hotmail.co.uk>
2021-01-31 20:25:58 +0000

    Using Promise.all to make multiple requests

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-import { GetBoardId, GetListsOnBoard, CreateCardForList } from "./trelloAPI.js";
+import {
+  GetBoardId,
+  GetListIdOnBoard,
+  CreateCardForList,
+  GetLabelIdOnBoard,
+} from "./trelloAPI.js";
+import fs from "fs";
 
 console.log("app.js running");
+
 GetBoardId("University - Sem 2").then((result) => {
-  GetListsOnBoard(result, "Backlog").then((result) => {
-    CreateCardForList(result, "Test List").then((result) => {
-      console.log(result);
+  const prom1 = GetLabelIdOnBoard(result, "CI660");
+  const prom2 = GetListIdOnBoard(result, "Backlog");
+  const prom3 = fs.promises.readFile("./weekNumber.json", "utf8");
+  Promise.all([prom1, prom2, prom3]).then((result) => {
+    let jsonString = JSON.parse(result[2]);
+    const createLectureCard = CreateCardForList(
+      result[1],
+      `CI660 Lecture ${jsonString.week}`,
+      result[0]
+    );
+    const createTutorialCard = CreateCardForList(
+      result[1],
+      `CI660 Tutorial ${jsonString.week}`,
+      result[0]
+    );
+    Promise.all([createLectureCard, createTutorialCard]).then((result) => {
+      jsonString.week += 1;
+      jsonString = JSON.stringify(jsonString);
+      fs.promises.writeFile("./weekNumber.json", jsonString, "utf8");
     });
   });
 });
--- trelloAPI.js
+++ trelloAPI.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import fs from "fs";
-import { resolve } from "path";
 
 const key = process.env.TRELLO_KEY;
 const token = process.env.TRELLO_TOKEN;
@@ -30,7 +28,7 @@ export const GetBoardId = (name) => {
   });
 };
 
-export const GetListsOnBoard = (boardID, listName) => {
+export const GetListIdOnBoard = (boardID, listName) => {
   return new Promise((resolve, reject) => {
     try {
       instance.get(`1/boards/${boardID}/lists`).then((result) => {
@@ -47,42 +45,33 @@ export const GetListsOnBoard = (boardID, listName) => {
   });
 };
 
-export const CreateCardForList = (listID, cardName) => {
+export const GetLabelIdOnBoard = (boardID, labelName) => {
   return new Promise((resolve, reject) => {
     try {
-      instance
-        .post(`1/cards`, null, { params: { idList: listID, name: cardName } })
-        .then((response) => resolve(response.data.id));
+      instance.get(`1/boards/${boardID}/labels`).then((result) => {
+        result.data.forEach((label) => {
+          if (label.name === labelName) {
+            resolve(label.id);
+          }
+        });
+        resolve(undefined);
+      });
     } catch (error) {
       reject(new Error(error));
     }
   });
 };
 
-let jsonData;
-
-// fs.readFile("./weekNumber.json", "utf8", (errors, data) => {
-//   jsonData = JSON.parse(data);
-//   console.log(jsonData);
-//   createCardForList(
-//     "5fb014e4235f1b4a300c8b09",
-//     `Complete CI609 Lectures, Week ${jsonData.week}`
-//   );
-//   createCardForList(
-//     "5fb014e4235f1b4a300c8b09",
-//     `Complete CI615 Lectures, Week ${jsonData.week}`
-//   );
-//   createCardForList(
-//     "5fb014e4235f1b4a300c8b09",
-//     `Complete CI646 Lectures, Week ${jsonData.week}`
-//   );
-//   jsonData.week = jsonData.week + 1;
-//   const jsonString = JSON.stringify(jsonData);
-//   fs.writeFile("./weekNumber.json", jsonString, "utf8", (err, data) =>
-//     console.log(data)
-//   );
-// });
-
-// getBoards()
-
-//getListOnBoard("5fb014d48b3b3b5c59736070");
+export const CreateCardForList = (listId, cardName, labelId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      instance
+        .post(`1/cards`, null, {
+          params: { idList: listId, name: cardName, idLabels: [labelId] },
+        })
+        .then((response) => resolve(response.data.id));
+    } catch (error) {
+      reject(new Error(error));
+    }
+  });
+};
